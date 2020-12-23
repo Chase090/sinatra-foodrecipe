632a786d44ffc943a198971a20c4ca768904c0b2
Chase090 <carloyam21@icloud.com>
2020-12-22 23:53:42 -0800

    add redirect for users laning page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,12 +1,33 @@
 class UserContoller < ApplicationController
 
-# * routes for login
+# * routes for login, reder login page
     get '/login' do 
         erb :login
+    end
+# * find the user, and login(create session)
+    post '/login' do 
+    # find the user
+        @user = User.find_by(email: params[:email])
+    # authenticate
+        if @user.authenticate(params[:password])
+            # log user in - create the user session
+            session[:user_id] = @user.id
+            # redirect to users landing page
+            puts session
+            redirect "users/#{@user.id}"
+        else
+        # tell the user invalid, then redirect to login page
+        end
+    # log in
+    
     end
 # *
     get '/signup' do 
 
+    end
+
+    get '/users/:id' do 
+        "show me somethin"
     end
 
 end
